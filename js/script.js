93ad778259f1104dc088e374504de567cafbdf02
memetibryan <memetibryan@gmail.com>
2022-01-16 16:23:22 +0300

    added code to query the database

--- js/script.js
+++ js/script.js
@@ -8,12 +8,22 @@ const client = new Client({
 })
 
 client.connect()
-.then(() => console.log("connected successfully"))
-.then(() => client.query("insert into data values ($3)", [234]))
-.then(() => client.query("select * from data"))
-.then(results => console.table(results.rows))
-.catch(e => console.log)
-.finally(() => client.end())
+
+client.query('select * from data', (err,res)=>{
+  if(!err){
+    console.log(res.rows);
+  }else {
+    console.log(err.message);
+  }
+  client.end;
+})
+
+// .then(() => console.log("connected successfully"))
+// .then(() => client.query("insert into data values ($3)", [234]))
+// .then(() => client.query("select * from data"))
+// .then(results => console.table(results.rows))
+// .catch(e => console.log)
+// .finally(() => client.end())
 
 // function saveData(){
 //   alert("SAVED TO DATABASE");
