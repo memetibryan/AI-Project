21bde434263af238533a443cae96c62000ffedc3
memetibryan <memetibryan@gmail.com>
2022-01-16 17:35:44 +0300

    node.js script to access database complete

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 const {Client} = require('pg');
-const client = new Client({
-  host: "localhost",
-  user: "postgres",
-  password: "postgres",
-  port: 5432,
-  database: "cheque",
-})
 
-client.connect()
+  const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    password: "postgres",
+    port: 5432,
+    database: "cheque",
+  })
 
-client.query('select * from data', (err,res)=>{
-  if(!err){
-    console.log(res.rows);
-  }else {
-    console.log(err.message);
-  }
-  client.end;
-})
+// code to query database data
+// client.query('select * from data', (err,res)=>{
+//   if(!err){
+//     console.log(res.rows);
+//   }else {
+//     console.log(err.message);
+//   }
+//   client.end;
+// })
 
-// .then(() => console.log("connected successfully"))
-// .then(() => client.query("insert into data values ($3)", [234]))
-// .then(() => client.query("select * from data"))
-// .then(results => console.table(results.rows))
-// .catch(e => console.log)
-// .finally(() => client.end())
+  client.connect()
 
-// function saveData(){
-//   alert("SAVED TO DATABASE");
-// }
+  .then(() => console.log("connected successfully"))
+  // .then(() => client.query("insert into data values ($1,$2,$3,$4)", ['1990-09-23','boni',4325,6789867]))
+  .then(() => client.query("select * from data"))
+  .then(results => console.table(results.rows))
+  .catch(e => console.log)
+  .finally(() => client.end())
+
+
+function saveData(){
+  alert("SAVED TO DATABASE");  
+}
