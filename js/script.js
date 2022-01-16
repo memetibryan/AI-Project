3940a2e44d43e974abf93e898cc92d6137ad8131
memetibryan <memetibryan@gmail.com>
2022-01-16 16:16:00 +0300

    successfully connected postgresql to node.js script called 'script.js'

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const {Client} = require('pg');
 const client = new Client({
+  host: "localhost",
   user: "postgres",
   password: "postgres",
-  host: "memetibryan",
   port: 5432,
   database: "cheque",
 })
 
 client.connect()
 .then(() => console.log("connected successfully"))
-.then(() => client.query("insert into information values ($3)", [234]))
-.then(() => client.query("select * from information"))
+.then(() => client.query("insert into data values ($3)", [234]))
+.then(() => client.query("select * from data"))
 .then(results => console.table(results.rows))
 .catch(e => console.log)
 .finally(() => client.end())
 
-function saveData(){
-  alert("SAVED TO DATABASE");
-}
+// function saveData(){
+//   alert("SAVED TO DATABASE");
+// }
