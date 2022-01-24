0842791a5b74f7d66fd051e1f02b3a295e9be945
memetibryan <memetibryan@gmail.com>
2022-01-24 15:07:38 +0300

    added express server script to load the HTML file

--- js/browserify.js
+++ js/browserify.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 3003;
 
-app.get('/',function(req, res) {
-    res.sendFile(__dirname + './index/index.html');
-    res.send()
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../index/index.html'));
 });
-app.listen(8000)
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
