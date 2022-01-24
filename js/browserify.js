9c5e1a45ae14720262778eb8f2d0b1c4072a5569
memetibryan <memetibryan@gmail.com>
2022-01-24 14:57:25 +0300

    added socket.io and express js packages

--- js/browserify.js
+++ js/browserify.js
@@ -1,2 +1,9 @@
-var foo = require('./database.js');
+var express = require('express');
+var app = express();
 
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + './index/index.html');
+    res.send()
+});
+app.listen(8000)
